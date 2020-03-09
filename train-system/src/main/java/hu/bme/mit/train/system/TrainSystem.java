3f336b3aa22c7d269af07cc6ef19812cee0cd52a
pelbal <pellesbalazs@gmail.com>
2020-03-09 08:30:56 -0700

    TravisError2

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -10,7 +10,7 @@ import hu.bme.mit.train.user.TrainUserImpl;
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
-	private TrainUser user = new TrainUserImpl(controller);
+	private TrainUser us er = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
 
 	public TrainController getController() {
