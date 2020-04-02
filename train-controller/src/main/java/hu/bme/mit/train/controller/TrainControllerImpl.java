f691a6e43852354fb6fe0879c53dd2218bf6e71d
pelbal <pellesbalazs@gmail.com>
2020-04-02 15:54:26 +0200

    KodValt

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,27 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+	Timer timer = new Timer();
+
+	public TrainControllerImpl(){
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				followSpeed();
+
+			}
+		}, 2*60*1000, 2*60*1000);
+	}
+
 
 	@Override
 	public void followSpeed() {
